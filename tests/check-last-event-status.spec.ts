e783a2ee10c7ed9fe16865e0ed51e73fae0575d2
alexandrerodgomesnet <alexandrerodgomes@hotmail.com>
2022-12-22 10:55:46 -0300

    ajustando status do evento (active e inReview)

--- tests/check-last-event-status.spec.ts
+++ tests/check-last-event-status.spec.ts
@@ -1,4 +1,15 @@
+import {set, reset} from 'mockdate';
+
 describe('CheckLastEventStatus', () => {
+
+    beforeAll(() =>{
+        set(new Date())
+    })
+
+    afterAll(() =>{
+        reset()
+    })
+
     it('should get last event data', async () => {
         const groupId: string = 'any_group_id';
         const { sut, loadLastEventRepository } = makeSut();
@@ -20,18 +31,46 @@ describe('CheckLastEventStatus', () => {
 
         expect(status).toBe('done');
     });
+
+    it('should return status active when now is before event end time', async () => {
+        const groupId: string = 'any_group_id';
+
+        const { sut, loadLastEventRepository } = makeSut();
+
+        loadLastEventRepository.output = {
+            endDate: new Date(new Date().getTime() + 1)
+        };
+
+        const status = await sut.perform(groupId);
+
+        expect(status).toEqual('active');
+    });
+
+    it('should return status inReview when now is after event end time', async () => {
+        const groupId: string = 'any_group_id';
+
+        const { sut, loadLastEventRepository } = makeSut();
+
+        loadLastEventRepository.output = {
+            endDate: new Date(new Date().getTime() - 1)
+        };
+
+        const status = await sut.perform(groupId);
+
+        expect(status).toEqual('inReview');
+    });
 });
 
 interface ILoadLastEventRepository {
-    loadLastEvent: (groupId: string) => Promise<undefined>;
+    loadLastEvent: (groupId: string) => Promise<{endDate: Date} | undefined>;
 }
 
 class LoadLastEventRepositoryMock implements ILoadLastEventRepository {
     groupId?: string;
     callsCount = 0;
-    output: undefined;
+    output?: {endDate: Date} | undefined;
 
-    async loadLastEvent(groupId: string): Promise<undefined> {
+    async loadLastEvent(groupId: string): Promise<{endDate: Date} | undefined> {
         this.groupId = groupId;
         this.callsCount++;
         return this.output;
@@ -42,8 +81,12 @@ class CheckLastEventStatus {
     constructor(private readonly loadLastEventRepository: ILoadLastEventRepository){}
 
     async perform(groupId: string): Promise<string> {
-        this.loadLastEventRepository.loadLastEvent(groupId);
-        return 'done';
+        const event = await this.loadLastEventRepository.loadLastEvent(groupId);
+
+        if(event === undefined) return 'done';
+
+        const now = new Date();
+        return (event?.endDate as Date) > now ? 'active' : 'inReview';
     }
 
 }
