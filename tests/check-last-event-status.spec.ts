41d70410c429c8889c17f5e99a646d83d7aae498
alexandrerodgomesnet <alexandrerodgomes@hotmail.com>
2022-12-22 10:09:56 -0300

    retorno de status do evento.

--- tests/check-last-event-status.spec.ts
+++ tests/check-last-event-status.spec.ts
@@ -2,34 +2,50 @@ describe('CheckLastEventStatus', () => {
     it('should get last event data', async () => {
         const groupId: string = 'any_group_id';
         const loadLastEventRepository = new LoadLastEventRepositoryMock();
-        const checkLastEventStatus = new CheckLastEventStatus(loadLastEventRepository);
 
-        await checkLastEventStatus.perform(groupId);
+        const sut = new CheckLastEventStatus(loadLastEventRepository);
+
+        await sut.perform(groupId);
 
         expect(loadLastEventRepository.groupId).toBe(groupId);
         expect(loadLastEventRepository.callsCount).toBe(1);
     });
+
+    it('should return status done when group has no event', async () => {
+        const groupId: string = 'any_group_id';
+        const loadLastEventRepository = new LoadLastEventRepositoryMock();
+        loadLastEventRepository.output = undefined;
+
+        const sut = new CheckLastEventStatus(loadLastEventRepository);
+
+        const status = await sut.perform(groupId);
+
+        expect(status).toBe('done');
+    });
 });
 
 interface ILoadLastEventRepository {
-    loadLastEvent: (groupId: string) => Promise<void>;
+    loadLastEvent: (groupId: string) => Promise<undefined>;
 }
 
 class LoadLastEventRepositoryMock implements ILoadLastEventRepository {
     groupId?: string;
     callsCount = 0;
+    output: undefined;
 
-    async loadLastEvent(groupId: string): Promise<void> {
+    async loadLastEvent(groupId: string): Promise<undefined> {
         this.groupId = groupId;
         this.callsCount++;
+        return this.output;
     }
 }
 
 class CheckLastEventStatus {
     constructor(private readonly loadLastEventRepository: ILoadLastEventRepository){}
 
-    async perform(groupId: string): Promise<void> {
+    async perform(groupId: string): Promise<string> {
         this.loadLastEventRepository.loadLastEvent(groupId);
+        return 'done';
     }
 
 }
